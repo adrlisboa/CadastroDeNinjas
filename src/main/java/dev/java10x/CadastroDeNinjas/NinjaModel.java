861145d6d2045c302ccfcf2031079a5df112a322
Adriano L. <adriano.psv@hotmail.com>
2026-03-19 21:50:23 -0300

    Transformando classe em entidade e colocando Id

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,51 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+// transformando uma classe em entidade
+@Entity
+// nome da tabela
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    // gerando Id automaticamente, sempre colocar acima do atributo para indicar o id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
